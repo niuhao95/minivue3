840969e13c215da20a3d0f2b01f43eaf704fcc3a
niuhao19 <niuhao19@jd.com>
2022-05-31 23:55:32 +0800

    chore: vitest试用

---
+++ packages/reactivity/__test__/demo.test.ts
@@ -0,0 +1,15 @@
+import {describe, it, expect} from 'vitest'
+
+function add (a,b){
+  return Number(a)+Number(b)
+}
+
+describe('试用vitest', () => {
+  it('测试add函数', () => {
+    // expect(add(1,2)).toEqual(3)
+    expect(add(1,2)).toBe(3)
+  })
+  it('测试add传入字符串', () => {
+    expect(add('1','2')).toBe(3)
+  })
+})
